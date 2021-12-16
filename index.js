b6b29f69a681631387ce2661358b5aeeca22970b
Tuchchapon <35054396+tuchchapon@users.noreply.github.com>
2021-12-17 05:53:35 +0700

    cors

--- index.js
+++ index.js
@@ -15,24 +15,24 @@ const mongoose = require('mongoose')
 //     origin: '*'
 // }));
 
-// app.use(function (req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header(
-//       "Access-Control-Allow-Headers",
-//       "Origin, X-Requested-With, Content-Type, Accept, Authorization , refreshToken"
-//     );
-//     res.header("Referrer-Policy", "no-referrer-when-downgrade");
-//     res.header("X-Content-Type-Options", "nosniff");
-//     res.header("X-XSS-Protection", "1; mode=block");
-//     res.header("X-Frame-Options", "SAMEORIGIN");
-//     res.header('Access-Control-Allow-Methods','POST, GET, PUT, PATCH, DELETE, OPTIONS')
-//     res.header("Strict-Transport-Security", "max-age=31536000; includeSubDomains");
-//     if (req.method === "OPTIONS") {
-//       res.header("Access-Control-Allow-Methods", "PUT, POST, PATH, DELETE, GET");
-//       return res.status(200).json({});
-//     }
-//     next();
-//   });
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization , refreshToken"
+    );
+    res.header("Referrer-Policy", "no-referrer-when-downgrade");
+    res.header("X-Content-Type-Options", "nosniff");
+    res.header("X-XSS-Protection", "1; mode=block");
+    res.header("X-Frame-Options", "SAMEORIGIN");
+    res.header('Access-Control-Allow-Methods','POST, GET, PUT, PATCH, DELETE, OPTIONS')
+    res.header("Strict-Transport-Security", "max-age=31536000; includeSubDomains");
+    if (req.method === "OPTIONS") {
+      res.header("Access-Control-Allow-Methods", "PUT, POST, PATH, DELETE, GET");
+      return res.status(200).json({});
+    }
+    next();
+  });
 //   app.all('*', function(req, res, next) {
 //     res.header("Access-Control-Allow-Origin", "*");
 //     res.header("Access-Control-Allow-Headers", "X-Requested-With");
