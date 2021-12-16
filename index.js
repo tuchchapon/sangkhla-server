b60d066fb97ff63812b2d381c19519db3acd451c
Tuchchapon <35054396+tuchchapon@users.noreply.github.com>
2021-12-17 05:08:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 const multer  = require('multer')
-const cors = require('cors')
+// const cors = require('cors')
 // const { dirname } = require('path');
 const path = require('path')
 // const appDir = dirname(require.main.filename);
@@ -11,26 +11,26 @@ app.use('/uploads', express.static(path.join(__dirname, 'public')));
 
 require('dotenv').config()
 const mongoose = require('mongoose')
-// app.use(cors({
-//     origin: 'https://www.section.io'
-// }))
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization , refreshToken"
-    );
-    res.header("Referrer-Policy", "no-referrer-when-downgrade");
-    res.header("X-Content-Type-Options", "nosniff");
-    res.header("X-XSS-Protection", "1; mode=block");
-    res.header("X-Frame-Options", "SAMEORIGIN");
-    res.header("Strict-Transport-Security", "max-age=31536000; includeSubDomains");
-    if (req.method === "OPTIONS") {
-      res.header("Access-Control-Allow-Methods", "PUT, POST, PATH, DELETE, GET");
-      return res.status(200).json({});
-    }
-    next();
-  });
+app.use(cors({
+    origin: 'https://www.section.io'
+}))
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header(
+//       "Access-Control-Allow-Headers",
+//       "Origin, X-Requested-With, Content-Type, Accept, Authorization , refreshToken"
+//     );
+//     res.header("Referrer-Policy", "no-referrer-when-downgrade");
+//     res.header("X-Content-Type-Options", "nosniff");
+//     res.header("X-XSS-Protection", "1; mode=block");
+//     res.header("X-Frame-Options", "SAMEORIGIN");
+//     res.header("Strict-Transport-Security", "max-age=31536000; includeSubDomains");
+//     if (req.method === "OPTIONS") {
+//       res.header("Access-Control-Allow-Methods", "PUT, POST, PATH, DELETE, GET");
+//       return res.status(200).json({});
+//     }
+//     next();
+//   });
 app.use(express.json());
 const mongoURL = process.env.DB_URL
 mongoose.connect(mongoURL,{
