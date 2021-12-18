bb311282ef4f35ae5b67d92a28d372820b14bc6f
Tuchchapon <35054396+tuchchapon@users.noreply.github.com>
2021-12-18 19:23:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ app.use('/uploads', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 const mongoose = require('mongoose')
-
 // app.use(cors({
 //     origin: '*'
 // }));
