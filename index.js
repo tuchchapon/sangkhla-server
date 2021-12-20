c452f01a7d0c8f8d393ccdc27b2da7534dac9b88
Tuchchapon <35054396+tuchchapon@users.noreply.github.com>
2021-12-20 19:01:35 +0700

    add img

--- index.js
+++ index.js
@@ -41,12 +41,12 @@ const mongoose = require('mongoose')
           }
           next();
         });
-        app.all('*', function(req, res, next) {
-          res.header("Access-Control-Allow-Origin", "*");
-          res.header("Access-Control-Allow-Headers", "X-Requested-With");
-          res.header('Access-Control-Allow-Headers', 'Content-Type');
-          next();
-        });
+        // app.all('*', function(req, res, next) {
+        //   res.header("Access-Control-Allow-Origin", "*");
+        //   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        //   res.header('Access-Control-Allow-Headers', 'Content-Type');
+        //   next();
+        // });
         
         // app.use(express.json());
         app.use('/',require('./pages/api/sangkhlaAPI'))
