a4626563bd8438b5f612e1aae607b11334a64c99
Tuchchapon <35054396+tuchchapon@users.noreply.github.com>
2021-12-21 15:24:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ const mongoURL = "mongodb+srv://admin:1234@sangkhla.lm5wh.mongodb.net/Sangkhla2g
 const mongoose = require('mongoose')
 
 const appDir = dirname(require.main.filename);
-app.use(cors({
-      origin: '*'
-  }));
-  app.use(bodyParser.json())
+// app.use(cors({
+//       origin: '*'
+//   }));
+//   app.use(bodyParser.json())
   // app.use(
   //     cors({
   //         origin: true,
@@ -26,30 +26,30 @@ app.use(cors({
   //         optionsSuccessStatus: 200,
   //       }),
   //     );
-      // app.use(function (req, res, next) {
-      //   res.header("Access-Control-Allow-Origin", "*");
-      //   res.header(
-      //     "Access-Control-Allow-Headers",
-      //     "Origin, X-Requested-With, Content-Type, Accept, Authorization , refreshToken"
-      //     );
-      //     res.header("Referrer-Policy", "no-referrer-when-downgrade");
-      //     res.header("X-Content-Type-Options", "nosniff");
-      //     res.header("X-XSS-Protection", "1; mode=block");
-      //     res.header("X-Frame-Options", "SAMEORIGIN");
-      //     res.header('Access-Control-Allow-Methods','POST, GET, PUT, PATCH, DELETE, OPTIONS')
-      //     res.header("Strict-Transport-Security", "max-age=31536000; includeSubDomains");
-      //     if (req.method === "OPTIONS") {
-      //       res.header("Access-Control-Allow-Methods", "PUT, POST, PATH, DELETE, GET");
-      //       return res.status(200).json({});
-      //     }
-      //     next();
-      //   });
-      //   app.all('*', function(req, res, next) {
-      //     res.header("Access-Control-Allow-Origin", "*");
-      //     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-      //     res.header('Access-Control-Allow-Headers', 'Content-Type');
-      //     next();
-      //   });
+      app.use(function (req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header(
+          "Access-Control-Allow-Headers",
+          "Origin, X-Requested-With, Content-Type, Accept, Authorization , refreshToken"
+          );
+          res.header("Referrer-Policy", "no-referrer-when-downgrade");
+          res.header("X-Content-Type-Options", "nosniff");
+          res.header("X-XSS-Protection", "1; mode=block");
+          res.header("X-Frame-Options", "SAMEORIGIN");
+          res.header('Access-Control-Allow-Methods','POST, GET, PUT, PATCH, DELETE, OPTIONS')
+          res.header("Strict-Transport-Security", "max-age=31536000; includeSubDomains");
+          if (req.method === "OPTIONS") {
+            res.header("Access-Control-Allow-Methods", "PUT, POST, PATH, DELETE, GET");
+            return res.status(200).json({});
+          }
+          next();
+        });
+        // app.all('*', function(req, res, next) {
+        //   res.header("Access-Control-Allow-Origin", "*");
+        //   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        //   res.header('Access-Control-Allow-Headers', 'Content-Type');
+        //   next();
+        // });
         
         app.use(express.json());
         app.use('/',require('./pages/api/sangkhlaAPI'))
